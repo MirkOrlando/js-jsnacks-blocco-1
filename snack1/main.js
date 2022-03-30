fc6f3d0d85575b510652762ec172f22b7d9f009b
MirkOrlando <mirko@orlando-m.it>
2022-03-30 10:52:47 +0200

    created snack2

--- snack1/main.js
+++ snack1/main.js
@@ -1,3 +1,8 @@
+/* SNACK 1 
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+ */
+
 const numb1 = parseInt(prompt("inserisci un numero"));
 const numb2 = parseInt(prompt("inserisci un altro numero"));
 
@@ -5,7 +10,7 @@ console.log(numb1, numb2);
 
 if (isNaN(numb1) || isNaN(numb2)) {
   alert(
-    "Inserire solo dati numerici! Cortesement ricaricare la pagina. Grazie"
+    "Inserire solo dati numerici! Cortesemente ricaricare la pagina. Grazie"
   );
 } else if (numb1 > numb2) {
   console.log(`il numero maggiore è ${numb1}`);
---
+++ snack2/main.js
@@ -0,0 +1,30 @@
+/* SNACK 2
+l'utente inserisce due parole in successione con due prompt.
+Il softwarte stampa prima la parola più corta, poi la più lunga
+ */
+
+const word1 = prompt("inserisci una parola");
+const word2 = prompt("inserisci un'altra parola");
+
+let wordLonger;
+let wordShorter;
+let sameLength = false;
+
+if (word1.length > word2.length) {
+  wordLonger = word1;
+  wordShorter = word2;
+} else if (word2.length > word1.length) {
+  wordLonger = word2;
+  wordShorter = word1;
+} else if (word1.length === word2.length) {
+  sameLength = true;
+}
+
+if (sameLength) {
+  console.log(
+    `La parola ${word1} e la parola ${word2} hanno la stessa lunghezza`
+  );
+} else {
+  console.log(wordLonger);
+  console.log(wordShorter);
+}
