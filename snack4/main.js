80e971e3dd37097f2b568c242dacd1dafa89b637
MirkOrlando <mirko@orlando-m.it>
2022-03-30 12:19:07 +0200

    created software snack4

---
+++ snack4/main.js
@@ -0,0 +1,31 @@
+/* SNACK 4
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+chiedi all’utente il suo nome e
+comunicagli se può partecipare o no alla festa.
+ */
+
+const guestsList = [
+  "luca",
+  "marco",
+  "claudio",
+  "stefano",
+  "lucia",
+  "alice",
+  "simona",
+];
+
+const userName = prompt("inserisci il tuo nome");
+let find = false;
+
+for (let i = 0; i < guestsList.length; i++) {
+  const guestName = guestsList[i];
+  if (userName === guestName) {
+    find = true;
+  }
+}
+
+if (find) {
+  console.log("Benvenuto alla festa! Divertiti");
+} else {
+  console.log("Mi spiace, ma il tuo nome non è sulla lista.");
+}
