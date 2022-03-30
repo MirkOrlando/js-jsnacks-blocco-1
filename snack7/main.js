e19777519081abb5ab0e08d588864cfee6220d6c
MirkOrlando <mirko@orlando-m.it>
2022-03-30 14:40:37 +0200

    created snack7

--- snack7/main.js
+++ snack7/main.js
@@ -1,1 +1,10 @@
-/* SNACK 7 */
+/* SNACK 7 
+Stampa le potenze di 2 fino a 1000.*/
+
+const base = 2;
+let power;
+
+for (let i = 0; i <= 1000; i++) {
+  power = base ** i;
+  console.log(`Se la è ${base} e l'esponente è ${i} il risultato è ${power}`);
+}
