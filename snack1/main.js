3c84356d7e314b1a7eb093cc8b29a0952f5153f7
MirkOrlando <mirko@orlando-m.it>
2022-03-30 10:27:23 +0200

    created message for NaN

--- snack1/main.js
+++ snack1/main.js
@@ -3,7 +3,11 @@ const numb2 = parseInt(prompt("inserisci un altro numero"));
 
 console.log(numb1, numb2);
 
-if (numb1 > numb2) {
+if (isNaN(numb1) || isNaN(numb2)) {
+  alert(
+    "inserire solo dati numerici! Cortesement ricaricare la pagina. Grazie"
+  );
+} else if (numb1 > numb2) {
   console.log(`il numero maggiore è ${numb1}`);
 } else if (numb2 > numb1) {
   console.log(`il numero maggiore è ${numb2}`);
