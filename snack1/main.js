88b99afdfa2f7ec3a119c002ef41e3a5b7007651
MirkOrlando <mirko@orlando-m.it>
2022-03-30 10:34:36 +0200

    corrected messages

--- snack1/main.js
+++ snack1/main.js
@@ -5,12 +5,12 @@ console.log(numb1, numb2);
 
 if (isNaN(numb1) || isNaN(numb2)) {
   alert(
-    "inserire solo dati numerici! Cortesement ricaricare la pagina. Grazie"
+    "Inserire solo dati numerici! Cortesement ricaricare la pagina. Grazie"
   );
 } else if (numb1 > numb2) {
   console.log(`il numero maggiore è ${numb1}`);
 } else if (numb2 > numb1) {
   console.log(`il numero maggiore è ${numb2}`);
 } else {
-  console.log(`i due numeri inseriti uguali`);
+  console.log(`i due numeri inseriti sono uguali`);
 }
