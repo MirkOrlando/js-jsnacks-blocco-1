d05f7057f5bc557ca0522bac2a520a60347a1a57
MirkOrlando <mirko@orlando-m.it>
2022-03-30 11:57:35 +0200

    working on snack3

--- snack3/main.js
+++ snack3/main.js
@@ -3,7 +3,7 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
  */
 
-let numbersList;
+let numbersList = [];
 let sum;
 
 for (let i = 0; i < 10; i++) {
@@ -11,7 +11,8 @@ for (let i = 0; i < 10; i++) {
 }
 
 for (let i = 0; i < numbersList.length; i++) {
-  sum += numbersList[i];
+  console.log(numbersList[i]);
+  sum += parseInt(numbersList[i]);
 }
 
 console.log(sum);
