03a854e966ad9dd3784cad4ac0645c3401508c16
MirkOrlando <mirko@orlando-m.it>
2022-03-30 12:08:38 +0200

    added if

--- snack3/main.js
+++ snack3/main.js
@@ -5,14 +5,23 @@ Il programma stampa la somma di tutti i numeri inseriti.
 
 let numbersList = [];
 let sum = 0;
+let error = false;
 
 for (let i = 0; i < 10; i++) {
   numbersList.push(parseInt(prompt("inserisci un numero")));
 }
 
 for (let i = 0; i < numbersList.length; i++) {
-  console.log(numbersList[i]);
-  sum += numbersList[i];
+  if (isNaN(numbersList[i])) {
+    error = true;
+  } else {
+    console.log(numbersList[i]);
+    sum += numbersList[i];
+  }
 }
 
-console.log(sum);
+if (error) {
+  console.log("uno o più dati inseriti non sono numerici");
+} else {
+  console.log(`la somma totale dei numeri inseriti è ${sum}`);
+}
