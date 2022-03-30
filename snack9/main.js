4a22b7e2d0aacb444f0bff4a339b97da1a15e0ec
MirkOrlando <mirko@orlando-m.it>
2022-03-30 15:33:27 +0200

    created snack9

--- snack9/main.js
+++ snack9/main.js
@@ -1,1 +1,14 @@
-/* SNACK 9 */
+/* SNACK 9
+Calcola la somma e la media dei primi 10 numeri. */
+
+let sum = 0;
+let average;
+const n = 10;
+
+for (let i = 1; i <= n; i++) {
+  sum += i;
+  average = sum / n;
+  console.log(i);
+}
+
+console.log(sum, average);
