14927a1a62604e66832eabad8457de1e1afcfffc
MirkOrlando <mirko@orlando-m.it>
2022-03-31 11:51:55 +0200

    created snacl3-while

---
+++ snack3/snack3-while/main.js
@@ -0,0 +1,39 @@
+/* SNACK 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+ */
+
+let sum = 0;
+let i = 0;
+
+while (i < 10) {
+  const userNumber = parseInt(prompt("inserisci un numero"));
+  sum += userNumber;
+  i++;
+}
+
+console.log(sum);
+
+/* let numbersList = [];
+let sum = 0;
+let error = false;
+
+for (let i = 0; i < 10; i++) {
+  numbersList.push(parseInt(prompt("inserisci un numero")));
+}
+
+for (let i = 0; i < numbersList.length; i++) {
+  if (isNaN(numbersList[i])) {
+    error = true;
+  } else {
+    console.log(numbersList[i]);
+    sum += numbersList[i];
+  }
+}
+
+if (error) {
+  console.log("uno o più dati inseriti non sono numerici");
+} else {
+  console.log(`la somma totale dei numeri inseriti è ${sum}`);
+}
+ */
